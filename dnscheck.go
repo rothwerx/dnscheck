f0ccf1b6bd4821ef501c0766350f9174fee70f61
Jeremiah Roth <jeremiah.roth@gmail.com>
2015-11-07 20:13:14 -0700

    Some refactoring, add sender as an option

--- dnscheck.go
+++ dnscheck.go
@@ -9,32 +9,60 @@ import (
 	"net/smtp"
 )
 
+type Opts struct {
+	email        string
+	sender       string
+	hostname     string
+	ipAddr       string
+	expectedAddr string
+}
+
+func sendemail(s Opts) int {
+	c, err := smtp.Dial("localhost:25")
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+	defer c.Close()
+
+	c.Mail(s.sender)
+	c.Rcpt(s.email)
+	wc, err := c.Data()
+	if err != nil {
+		log.Fatal(err)
+		return 1
+	}
+	defer wc.Close()
+	ns := fmt.Sprintf("Address %s is resolving to %s\n",
+		s.hostname, s.expectedAddr)
+	buf := bytes.NewBufferString(ns)
+	if _, err = buf.WriteTo(wc); err != nil {
+		log.Fatal(err)
+		return 1
+	}
+	return 0
+}
+
 func main() {
 	hostPtr := flag.String("hostname", "google.com", "Hostname to resolve")
 	ipPtr := flag.String("ip", "216.58.216.142", "Expected IP")
-	emailPtr := flag.String("email", "", "Email to send failures to")
+	emailPtr := flag.String("email", "", "Email address to send failures")
+	senderPtr := flag.String("sender", "example@example.com", "Email address to send as")
 	flag.Parse()
 
 	netaddr, _ := net.ResolveIPAddr("ip4", *hostPtr)
-	if netaddr.String() != *ipPtr {
-		fmt.Printf("%s\n", netaddr)
 
-		c, err := smtp.Dial("localhost:25")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer c.Close()
-
-		c.Mail("wupadmin@wildutahproject.org")
-		c.Rcpt(*emailPtr)
-		wc, err := c.Data()
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer wc.Close()
-		buf := bytes.NewBufferString(netaddr.String())
-		if _, err = buf.WriteTo(wc); err != nil {
-			log.Fatal(err)
-		}
+	ns := Opts{
+		email:        *emailPtr,
+		sender:       *senderPtr,
+		hostname:     *hostPtr,
+		ipAddr:       *ipPtr,
+		expectedAddr: netaddr.String(),
 	}
+
+	if netaddr.String() != *ipPtr {
+		fmt.Printf("%s\n", ns.expectedAddr)
+		sendemail(ns)
+	}
+
 }
